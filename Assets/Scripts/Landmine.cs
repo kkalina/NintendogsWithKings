b17c0147f2d4c0e399bfe69b739d1fea7c67b923
Kelvin Tam <kelvin.tam@live.com>
2016-10-14 22:05:37 -0400

    landmine

---
+++ Assets/Scripts/Landmine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Landmine : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		explosion.SetActive(true);
+		GetComponent<Renderer>().enabled = false;
+		Invoke("DestroyLandmine", 2);
+	}
+
+	void DestroyLandmine() {
+		Destroy(gameObject);
+	}
+}
