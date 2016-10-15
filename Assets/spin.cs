c767f77bfa92c7b0576721add2e7f249104da8ae
kkalina <kkalina@umich.edu>
2016-10-15 00:05:34 -0400

    landMineLauncherTest

---
+++ Assets/spin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+
+    public float speed = 1f;
+    private float angle = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        angle += Time.deltaTime * speed;
+        this.transform.rotation = Quaternion.EulerAngles(0,angle,0);
+	}
+}
