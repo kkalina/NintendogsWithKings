18c04816e8bb65d4774f39505e6ece02bb18457e
kkalina <kkalina@umich.edu>
2016-10-15 19:43:19 -0400

    sticky landmines

---
+++ Assets/landmineStick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class landmineStick : MonoBehaviour {
+
+    private bool stuck = false;
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!stuck)
+        {
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point;
+            this.transform.rotation = rot;
+            pos += this.transform.up*0.3f;
+            this.transform.position = pos;
+            this.transform.SetParent(collision.gameObject.transform);
+            Destroy(this.gameObject.GetComponent<Rigidbody>());
+            stuck = true;
+        }
+    }
+    
+
+}
