398512b043b614d63de9e0b202e4cf52d7aa2567
Ben <coobenja@umich.edu>
2016-10-14 22:52:26 -0400

    Peasant can now ragdoll

---
+++ Assets/_Scripts/Ragdoller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ragdoller : MonoBehaviour {
+
+    public GameObject ragdoll;
+    SkinnedMeshRenderer skin;
+
+	// Use this for initialization
+	void Start () {
+        skin = transform.FindChild("Character").gameObject.GetComponent<SkinnedMeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1)) {
+            skin.enabled = false;
+            Instantiate(ragdoll, transform.position, Quaternion.identity);
+        }
+    }
+}
