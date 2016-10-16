b6b3cf654457b43b7d6ff61427e215f7f35c11e9
Becarls <becarls@umich.edu>
2016-10-16 12:46:37 -0400

    Loading Scene Script

---
+++ Assets/_Scripts/LoadNextLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour {
+
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("load damn it");
+        if (other.tag == "Player") {
+            
+            SceneManager.LoadScene("Karls_Kastle_Test");
+            
+        }
+    }
+}
