938d0015ca72779cf0dd219941a8b716e23f8dc9
kkalina <kkalina@umich.edu>
2016-10-16 14:18:46 -0400

    scripts

---
+++ Assets/_Scripts/KingHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KingHealth : MonoBehaviour {
+    private TextMesh healthDisplay;
+    private GameObject playerObj;
+    private FPSControl playerController;
+    GameObject king;
+    public float health;
+
+	// Use this for initialization
+	void Start () {
+        playerObj = GameObject.Find("FPS_Player");
+        playerController = playerObj.GetComponent<FPSControl>();
+        king = playerController.King;
+        health = king.GetComponent<KingNav>().health;
+        healthDisplay = this.gameObject.GetComponent<TextMesh>();
+        healthDisplay.text = health.ToString("F2");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        health = king.GetComponent<KingNav>().health;
+        healthDisplay.text = health.ToString("F2");
+	}
+}
