42b930bf2bb911ae59262d91ca50b15a042bfc70
kkalina <kkalina@umich.edu>
2016-10-15 20:40:48 -0400

    logistics, score tallying

---
+++ Assets/score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score : MonoBehaviour {
+
+    public int currScore;
+    public int dispScore;
+    private TextMesh scoreBoard;
+    private GameObject playerObj;
+    private FPSControl playerController;
+
+	// Use this for initialization
+	void Start () {
+        playerObj = GameObject.Find("FPS_Player");
+        playerController = playerObj.GetComponent<FPSControl>();
+        scoreBoard = this.gameObject.GetComponent<TextMesh>();
+        dispScore = playerController.damage;
+        scoreBoard.text = "$" + dispScore.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currScore = playerController.damage;
+        if (currScore > dispScore)
+        {
+            dispScore += (int)Mathf.Ceil(((currScore + dispScore) *.0005f));
+            scoreBoard.text = "$" + dispScore.ToString();
+        }
+	}
+}
