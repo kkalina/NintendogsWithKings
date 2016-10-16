7e56dac907d64217c42d0577b0a260b85e4a4882
Ben <coobenja@umich.edu>
2016-10-15 20:41:22 -0400

    Changed FPS Player and cannon

--- Assets/_Scripts/Shoot.cs
+++ Assets/_Scripts/Shoot.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class Shoot : MonoBehaviour {
     public GameObject bullet;
     public Transform fire_point;
+    bool shooting = false;
 
     void Start() {
         fire_point = transform.FindChild("Fire_Point").transform;
@@ -13,11 +14,18 @@ public class Shoot : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            Instantiate(bullet, fire_point.position + fire_point.forward*1.5f , fire_point.rotation);
-            this.transform.DOShakeScale(.4f, new Vector3(0, 0, 2f), 10, 1, true);
-            this.transform.DOPunchRotation(new Vector3(90f, 10f, 10f), .4f, 1, .1f);
-            this.transform.DOPunchPosition(new Vector3(0f, 0f, -.8f), .4f, 1, 1, false);
+        if (Input.GetMouseButtonDown(0) && !shooting) {
+            StartCoroutine(shoot());
         }
 	}
+
+    IEnumerator shoot() {
+        shooting = true;
+        Instantiate(bullet, fire_point.position + fire_point.forward * 1.5f, fire_point.rotation);
+        this.transform.DOShakeScale(.4f, new Vector3(0, 0, 2f), 10, 1, true);
+        this.transform.DOPunchRotation(new Vector3(90f, 10f, 10f), .4f, 1, .1f);
+        this.transform.DOPunchPosition(new Vector3(0f, 0f, -.8f), .4f, 1, 1, false);
+        yield return new WaitForSeconds(.5f);
+        shooting = false;
+    }
 }
