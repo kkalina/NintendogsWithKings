c5b9d19e1a4c352629149867529f0582b435f202
Becarls <becarls@umich.edu>
2016-10-16 16:32:41 -0400

    Pointer to king stuff, add to player, give it king transform

--- Assets/_Scripts/PointToKing.cs
+++ Assets/_Scripts/PointToKing.cs
@@ -3,22 +3,11 @@ using System.Collections;
 
 public class PointToKing : MonoBehaviour {
 
-    public Transform arrow;
     public Transform king;
-    public Transform player;
 
-    private Vector3 ray;
-    private RaycastHit hit;
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(Physics.Raycast(player.position, king.position, out hit)){
-            float angle = Mathf.Atan2(hit.point.y, hit.point.x) * Mathf.Rad2Deg;
-            arrow.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.LookAt(king);
+    }    
 }
