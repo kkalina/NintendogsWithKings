4bd31ff4387d8520a31c39b75d8a37ac4fc41cc7
kkalina <kkalina@umich.edu>
2016-10-15 21:26:17 -0400

    fixed parent-warping

--- Assets/_Scripts/landmineStick.cs
+++ Assets/_Scripts/landmineStick.cs
@@ -12,10 +12,15 @@ public class landmineStick : MonoBehaviour {
             ContactPoint contact = collision.contacts[0];
             Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
             Vector3 pos = contact.point;
-            this.transform.rotation = rot;
+            GameObject attachPoint = new GameObject();
+            attachPoint.transform.rotation = rot;
             pos += this.transform.up*0.3f;
-            this.transform.position = pos;
-            this.transform.SetParent(collision.gameObject.transform);
+            attachPoint.transform.position = pos;
+            attachPoint.transform.SetParent(collision.gameObject.transform);
+            attachPoint.transform.localScale = new Vector3(1, 1, 1);
+            this.transform.position = attachPoint.transform.position;
+            this.transform.rotation = attachPoint.transform.rotation;
+            this.transform.SetParent(attachPoint.transform);
             Destroy(this.gameObject.GetComponent<Rigidbody>());
             this.gameObject.GetComponent<AudioSource>().Play();
             stuck = true;
